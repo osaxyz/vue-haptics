71e8c107e5cfe5f68749f4d8ba28771d32f97531
Asuma Yamada <yamadaasuma@gmail.com>
2024-12-11 03:09:58 +0900

    chore: update Deno configuration and Vite setup

--- sample/vite.config.ts
+++ sample/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import deno from '@deno/vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [deno(), react()],
+  plugins: [deno() as PluginOption, react()],
 })
